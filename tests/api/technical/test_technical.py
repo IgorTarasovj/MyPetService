404a5bb7cded3aa6dde148fe32a88f2560aec71d
IgorTarasovj <49902036+IgorTarasovj@users.noreply.github.com>
2026-07-16 18:39:22 +0300

    change tests.yml add pytest.ini

--- tests/api/technical/test_technical.py
+++ tests/api/technical/test_technical.py
@@ -1,11 +1,11 @@
-import pytest
+import allure
 from http import HTTPStatus
 from src.clients.http_builder import get_http_client
 from src.tools.assertions.common import assert_status_code, validate_response
 from src.models.technical.technical import GetResponseStatusSchema
 
 
-
+@allure.title("Get service status")
 def test_status():
     api_client = get_http_client()
     response = api_client.get(url="/status")
